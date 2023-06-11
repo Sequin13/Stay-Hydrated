27557d43a083eaf9cedc8c2b35bf78ddb892a110
Sequin13 Waitinskyy <kubaczekajski@gmail.com>
2023-06-11 18:26:54 +0200

    to co grzana zrobil ez

--- app/src/main/java/com/example/projekt_apki/registerActivity.java
+++ app/src/main/java/com/example/projekt_apki/registerActivity.java
@@ -2,19 +2,24 @@ package com.example.projekt_apki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class registerActivity extends AppCompatActivity {
 
-    EditText ed, ed2, ed3, ed4, ed5;
+    EditText ed, ed2, ed3, ed4, ed5,ed6;
     DataBase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
+
+
     public void RegisterClick (View v)
     {
         database = new DataBase(this);
@@ -25,16 +30,24 @@ public class registerActivity extends AppCompatActivity {
         ed3=findViewById(R.id.wiek);
         ed4=findViewById(R.id.username);
         ed5=findViewById(R.id.plec);
+        ed6=findViewById(R.id.passwordconfirm);
 
-        String x, y, z, a, b;
+        String x, y, z, a, b,g;
         x=String.valueOf(ed.getText());
         y=String.valueOf(ed2.getText());
         z=String.valueOf(ed3.getText());
         a=String.valueOf(ed4.getText());
         b=String.valueOf(ed5.getText());
-
-
-        database.insertData(a, y, x, b, Integer.parseInt(z), 5000 );
+        g=String.valueOf(ed6.getText());
+        if(y.equals(g)){
+            database.insertData(a, y, x, b, Integer.parseInt(z), 5000 );
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+            Toast.makeText(getApplicationContext(), "Account Created !", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Check if confirm password and password are the same", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
